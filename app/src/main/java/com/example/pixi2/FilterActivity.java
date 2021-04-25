118b7c67885994e190ea9319793a623525608490
henrietta789 <56877018+henrietta789@users.noreply.github.com>
2021-04-25 14:37:55 +0300

    6

--- app/src/main/java/com/example/pixi2/FilterActivity.java
+++ app/src/main/java/com/example/pixi2/FilterActivity.java
@@ -2,13 +2,57 @@ package com.example.pixi2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class FilterActivity extends AppCompatActivity {
+    private static int SELECT_PICTURE = 200;
+    Button add, save;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+
+        add= findViewById(R.id.button_add);
+        save= findViewById(R.id.button_save);
+        img = findViewById(R.id.kep_imageView);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setType("image/*");
+                i.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // update the preview image in the layout
+                    img.setImageURI(selectedImageUri);
+                }
+            }
+        }
     }
 }
