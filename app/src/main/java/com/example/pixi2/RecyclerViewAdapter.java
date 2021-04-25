1ff49d2ad652396690228e697ab3e6ca8dd5e7f8
henrietta789 <56877018+henrietta789@users.noreply.github.com>
2021-04-25 16:48:41 +0300

    7

--- app/src/main/java/com/example/pixi2/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/pixi2/RecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.pixi2;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
